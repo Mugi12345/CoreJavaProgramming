d8482658aabfd71adbb2bdffdd982f2e3cf71626
Filip Mugosa <filipmugosa7@gmail.com>
2024-12-10 01:47:44 +0100

    inicijalizacija

---
+++ src/One/Program.java
@@ -0,0 +1,9 @@
+package One;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("kkakakakakak");
+        System.out.println("najrmaksja");
+    }
+}
